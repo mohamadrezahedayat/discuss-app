4ca27a5850c250eb42129030f77bd2e939c257f6
mohamadreza hedayatnia <mohamadrezahedayat@gmail.com>
2024-01-11 19:10:47 +0330

    database queries

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,72 @@
-'use server'
+"use server";
 
-export async function createPost(){
-  // todo: revalidate topic show page
+import type { Post, Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return { errors: { _form: ["You must be signed in to do this."] } };
+  }
+
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  const topic = await db.topic.findFirst({ where: { slug } });
+  if (!topic) {
+    return { errors: { _form: ["Cannot find topic"] } };
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        topicId: topic.id,
+        userId: session.user.id,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return { errors: { _form: [error.message] } };
+    } else {
+      return {
+        errors: {
+          _form: ["Faild to create post"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug,post.id));
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -12,42 +12,46 @@ import { useFormState } from "react-dom";
 import * as actions from "@/actions";
 import FormButton from "@/components/common/form-button";
 
-export default function TopicCreateForm() {
-  const [formState, action] = useFormState(actions.createTopic, {
+interface PostCreateFormProps {
+  slug: string;
+}
+
+export default function PostCreateForm({ slug }: PostCreateFormProps) {
+  const [formState, action] = useFormState(actions.createPost.bind(null,slug), {
     errors: {},
   });
-
   return (
     <Popover placement="left">
       <PopoverTrigger>
-        <Button color="primary">Create a Topic</Button>
+        <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create a Topic</h3>
+            <h3 className="text-lg">Create a Post</h3>
+
             <Input
-              name="name"
-              label="Name"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
+              name="title"
+              label="Title"
               labelPlacement="outside"
-              placeholder="Name"
-              isInvalid={!!formState.errors.name}
-              errorMessage={formState.errors.name?.join(", ")}
+              placeholder="Title"
             />
             <Textarea
-              name="description"
-              label="Description"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
+              name="content"
+              label="Content"
               labelPlacement="outside"
-              placeholder="Describe Your Topic"
-              isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(", ")}
+              placeholder="Content"
             />
             {formState.errors._form ? (
               <div className="rounded p-2 bg-red-200 border border-red-200">
                 {formState.errors._form.join(", ")}
               </div>
             ) : null}
-            <FormButton>Submit</FormButton>
+            <FormButton>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
